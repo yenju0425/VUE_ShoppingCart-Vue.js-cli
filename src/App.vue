976beb486104a14aa3dcbca086d78c20187c2f58
Rick Yellow <yenju0425@gmail.com>
2023-02-27 16:58:39 +0800

    Using Vue CLI to build the shopping cart app. init commit.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+
+    <!-- When the user clicks the cart icon, toggle the sidebar -->
+    <!-- <a @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </a> -->
+
+  </header>
+
   <router-view/>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './assets/styles/style.scss' // import the css file
+
 createApp(App).use(router).mount('#app')
